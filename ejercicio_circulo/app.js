9bd379622c67367548cc9010f0830a2d8627c7af
sofiafajardoro <sofia.fajardo.ro@gmail.com>
2025-12-03 23:29:20 -0300

    Delete ejercicio_circulo directory

--- ejercicio_circulo/app.js
+++
@@ -1,9 +0,0 @@
-// pido el diametro y calculo el área
-const d = Number(prompt("Ingrese el diámetro del círculo (cm):"));
-const r = d / 2;
-const area = Math.PI * Math.pow (r, 2);
-
-// salida en tres lugares
-console.log(`Area: ${area.toFixed(2)} cm².`);
-alert(`Area: ${area.toFixed(2)} cm².`);
-document.getElementById("resultado").textContent = `Area: ${area.toFixed(2)} cm².`;
